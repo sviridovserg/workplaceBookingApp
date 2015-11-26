230156e904cd33fc0a74fb0a21692b5be84d1cff
Sergey Sviridov <sviridovserg@gmail.com>
2015-11-26 20:26:46 +0300

    Violet color for booked days in schedule

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -29,11 +29,11 @@ module.exports = function(config) {
       'bower_components/angular-aria/angular-aria.js',
       'bower_components/angular-material/angular-material.js',
       'bower_components/angular-route/angular-route.js',
+      'bower_components/angular-translate/angular-translate.js',
       'bower_components/angular-ui-router/release/angular-ui-router.js',
       'bower_components/moment/moment.js',
       'bower_components/ngstorage/ngStorage.js',
       'bower_components/underscore/underscore.js',
-      'bower_components/angular-translate/angular-translate.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
